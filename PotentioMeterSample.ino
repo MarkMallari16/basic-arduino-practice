f9f6938b0eb03a22279697a82ce9ea5324a8af8a
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2026-08-14 15:26:44 +0800

    Add files via upload

---
+++ PotentioMeterSample.ino
@@ -0,0 +1,27 @@
+int LED_PIN = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+
+  int brightness = sensorValue / 4;
+
+  analogWrite(LED_PIN,brightness);
+
+  Serial.print(" ADC reading: ");
+  Serial.print(sensorValue);
+
+  Serial.println();
+
+  Serial.print("Analog voltage: ");
+  Serial.print(voltage);
+  delay(1);
+} 
